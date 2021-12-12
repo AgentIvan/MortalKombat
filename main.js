ad569e2de2bd868cd229ee700238e50ff511371d
Ivan D. WLS <I.Derzhylo@softpositive.com>
2021-12-12 13:07:24 -0600

    video 5

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 "use strict";
 
 
-const rand = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
+const rand = (max, min = 0) => Math.ceil(Math.random() * (max - min)) + min;
 
 const URLS = {
   "scorpion": "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   "kitana": "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
-  "liukang": "http://reactmarathon-api.herokuapp.com/assets/liukang.gif",
+  "liu kang": "http://reactmarathon-api.herokuapp.com/assets/liukang.gif",
   "sonya": "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
   "sub-zero": "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
 };
 URLS.sample = function () {
   const values = Object.values(this);
-  const casual = rand(values.length - 1);
+  const casual = rand(values.length) - 1;
   return values[casual];
 }
 
@@ -29,6 +29,7 @@ class Player {
     console.log(`fight ${this.name} Fight... ${n ?? ""}`);
   }
   constructor({ player = 1, name, img = URLS.sample(), hp = 100 }) {
+    this.player = player;
     this.name = name;
     let nameKey = name.toLowerCase(); //.replace(/[^\w]/gi, '');
     this.img = (nameKey in URLS) ? URLS[nameKey] : img;
@@ -36,30 +37,45 @@ class Player {
   }
 }
 
-const createPlayer = (p, player) => {
-  console.log({ p, player });
-  const pDiv = document.createElement('div');
-  pDiv.className = p;
-  pDiv.innerHTML = `
-    <div class="progressbar">
-      <div class="life" style="width:${player.hp}%"></div>
-      <div class="name">${player.name}</div>
-    </div>
-    <div class="character">
-      <img src="${player.img}" />
-    </div>`
-  return pDiv;
+const $arenas = document.querySelector("div.arenas");
+
+function createElement(tag, className) {
+  const $tag = document.createElement(tag);
+  if (className) {
+    $tag.classList.add(className);
+  }
+  return $tag;
+}
+
+const createPlayer = (playerObj) => {
+  console.log({ player: playerObj });
+  const $player = createElement('div', 'player' + playerObj.player);
+  const $progressbar = createElement('div', 'progressbar');
+  const $character = createElement('div', 'character');
+  const $life = createElement('div', 'life');
+  const $name = createElement('div', 'name');
+  const $img = createElement('img');
+
+  $life.style.width = playerObj.hp + '%';
+  $name.innerText = playerObj.name;
+  $img.src = playerObj.img;
+
+  $progressbar.appendChild($name);
+  $progressbar.appendChild($life);
+
+  $character.appendChild($img);
+  $player.appendChild($character);
+  $player.appendChild($progressbar);
+
+  return $player;
 };
 
 const init = () => {
   const player1 = new Player({ player: 1, name: "SCORPION", img: URLS.sample(), hp: 76 });
-  const playerDiv1 = createPlayer("player1", player1);
   const player2 = new Player({ player: 2, name: "SUB-ZERO", img: URLS.sample() });
-  const playerDiv2 = createPlayer("player2", player2);
 
-  const $arenas = document.querySelector("div.arenas");
-  $arenas.appendChild(playerDiv1);
-  $arenas.appendChild(playerDiv2);
+  $arenas.appendChild(createPlayer(player1));
+  $arenas.appendChild(createPlayer(player2));
 };
 
 init();
